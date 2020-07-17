70ab5aa78905437c25dd83f01b4f51272e4f67f6
Matt Fellenz <mattf53190@gmail.com>
2020-07-16 18:37:18 -0700

    Chore: update version number

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ function humanFileSize (bytes, si = false, dp = 1) {
  * @type {string}
  * @constant
  */
-const _version = '1.0.1';
+const _version = '1.1.0';
 document.title = `TagViewer ${_version}`;
 
 let config = {}; let cache = {}; const safeMode = [false, false];
