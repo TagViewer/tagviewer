a6e12b3df810b15354b6349629f68d6a8944af86
Matt Fellenz <mattf53190@gmail.com>
2020-08-08 11:57:12 -0700

    Fix #35
    This fixed the addMediaDialog method to add the _index property to file entries in the TagViewer metadata. That way various actions won't fail.

--- index.js
+++ index.js
@@ -1727,6 +1727,7 @@ const vm = new Vue({
             // the currentIndex is updated within the addMedia mutation called by the homonymous action.
             _path: (this.$store.state.tagviewerMeta.currentIndex) + path.extname(file),
             _origBasename: path.basename(file),
+            _index: this.$store.state.tagviewerMeta.currentIndex,
             tags: [],
             props: {},
             title: path.parse(file).name,
